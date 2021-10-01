2e2ef47a770711d90219d1574b714a8a19d4b693
Trust Wallet Viet Nam <88810693+trustwalletvn@users.noreply.github.com>
2021-10-01 23:05:55 +0700

    Update faucet.sol

--- smartcontract/faucet.sol
+++ smartcontract/faucet.sol
@@ -7,7 +7,7 @@ interface ERC20 {
 
 contract Faucet {
     uint256 constant public tokenAmount = 100000000000000000000;
-    uint256 constant public waitTime = 30 minutes;
+    uint256 constant public waitTime = 1 minutes;
 
     ERC20 public tokenInstance;
     
